8865bf1ba4914cc7d04eed8db5b8b155e7f225ba
Peter Tyldesley <ptyldesley@schawk.com>
2017-10-05 12:58:48 +0100

    Fix API calls

--- src/components/channel_item.js
+++ src/components/channel_item.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
 const ChannelItem = (props) => {
+  if (!props.stream) {
+    return (
+      <div>Loading...</div>
+    )
+  }
+
   const { channel } = props;
+  const { stream } = props;
+  let currentlyPlaying = "";
+
+  if (stream.stream) {
+    currentlyPlaying = stream.stream.channel.status;
+  }
+
+  const status = stream.stream ? 'online' : 'offline';
+  
   return (
-    <div>
+    <div className={status}>
       <img src={channel.logo} style={{width: 50}} className="img-circle" alt="Cinque Terre" />
       <span className="display-name">{channel.display_name}</span>
-      <span className="status">Online</span>
+      <span className="currently-playing">{currentlyPlaying}</span>
     </div>
   );
 }
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -6,13 +6,10 @@ import map from 'async/map';
 
 const channelNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-let channels = [];
-let streams = [];
-
 class TabNav extends Component {
   constructor(props) {
     super(props);
-    this.state = {key : 1, channels: [], streams: []};
+    this.state = {key : 1, channels: [], streams: [], online: [], offline: []};
     this.handleSelect = this.handleSelect.bind(this);
   }
 
@@ -24,11 +21,19 @@ class TabNav extends Component {
     var self = this;
     
     map(channelNames, getChannels, function (err, results) {
-      self.setState({channels: results})
-    });
+      self.setState({channels: results});
 
-    map(channelNames, getStreams, function (err, results) {
-      self.setState({streams: results})
+      map(channelNames, getStreams, function (err, results) {
+        self.setState({streams: results});
+  
+        results.forEach((stream) => {
+          if (stream.stream) {
+            self.setState({online: self.state.online.concat(stream.stream.channel)});
+          }
+        });
+  
+        self.setState({offline: self.state.channels.slice(self.state.online.length)});
+      });
     });
   }
 
@@ -47,7 +52,7 @@ class TabNav extends Component {
           <ChannelList 
             color="panel-success" 
             title="Online" 
-            channels={this.state.channels} 
+            channels={this.state.online} 
             streams={this.state.streams} 
           />
         </Tab>
@@ -55,8 +60,8 @@ class TabNav extends Component {
           <ChannelList 
             color="panel-info" 
             title="Offline" 
-            channels={this.state.channels} 
-            streams={this.state.streams} 
+            channels={this.state.offline} 
+            streams={this.state.streams.slice(this.state.online.length)} 
           />
         </Tab>
       </Tabs>
